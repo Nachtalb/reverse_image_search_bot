8df1f9b5a018951ba673330759357b442c10aa23
Nick Espig <nickespig@gmail.com>
2017-10-23 22:29:48 +0200

    Use other string format for the start / help message

--- animu_image_search_bot/commands.py
+++ animu_image_search_bot/commands.py
@@ -20,25 +20,46 @@ def start(bot: Bot, update: Update):
     """
     print(botan.track(BOTAN_API_TOKEN, update.message.from_user.id, update.message.to_dict(), '/start'))
 
-    reply = ('Send me images or stickers and I will send you direct reverse image search links for IQDB, Google, '
-             'TinEye and Bing. For anime images I recommend IQDB and TinEye, for other images I recommend to use '
-             'Google or TinEye.'
-             '\n\n*Features:*'
-             '\n- Give you image reverse search links'
-             '\n- Supports IQDB, Google, TinEye and Bing'
-             '\n- Supports normal images like JPG, PNG, WEBP'
-             '\n- Supports stickers'
-             '\n- Best Match information by IQDB'
-             '\n\n*ToDo:*'
-             '\n- Support for GIFs'
-             '\n- Best Match information by TinEye'
-             '\n\n*Commands:* The only commands I have are /help and /start which shows this message.'
-             '\n\n*Attention whore stuff*: Please share this bot with your friends so that I ('
-             '[the magician](https://github.com/Nachtalb/) behind this project) have enough motivation to continue and '
-             'maintain this bot.'
-             '\n\n*Contributions:* If you have found a bug or want a new feature, please make an issue here: '
-             '[Nachtalb/animu_image_search_bot](https://github.com/Nachtalb/animu_image_search_bot)'
-             '\n\nThank you for using @anime\_image\_search\_bot.')
+    reply = """*Anime Image Search Bot*
+
+[@anime_image_search_bot](https://t.me/anime_image_search_bot)
+
+*How to use me*
+Send me images or stickers and I will send you direct reverse image search links for IQDB, Google, TinEye and Bing. 
+For anime images I recommend IQDB and TinEye, for other images I recommend to use Google or TinEye.
+
+*Features*
+- Give you image reverse search links
+- Supports IQDB, Google, TinEye and Bing
+- Supports normal images like JPG, PNG, WEBP
+- Supports stickers
+- Best Match information by IQDB
+
+*ToDo*
+- Support for GIFs
+- Best Match information by TinEye
+
+*Commands*
+- /help, /start: show a help message with information about the bot and it's usage.
+
+*Attention whore stuff* 
+Please share this bot with your friends so that I ([the magician](https://github.com/Nachtalb/) behind this project) 
+have enough motivation to continue and maintain this bot.
+
+Check out my other project\[s\]: 
+- [@insta_looter_bot](https://github.com/Nachtalb/insta_looter_bot) - Download images and videos from Instagram via 
+Telegram
+
+
+*Contributions*
+_Bug report / Feature request_
+If you have found a bug or want a new feature, please make an issue here: [Nachtalb/animu_image_search_bot](https://github.com/Nachtalb/animu_image_search_bot)
+
+_Code Contribution / Pull Requests_
+Please use a line length of 120 characters and [Google Style Python Docstrings](http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html). 
+
+Thank you for using [@anime_image_search_bot](https://t.me/anime_image_search_bot).
+"""
 
     update.message.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
 
