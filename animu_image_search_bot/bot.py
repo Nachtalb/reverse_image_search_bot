91bd2c24c1aceadbe0873eda6b51194f7db835ab
Nick Espig <nickespig@gmail.com>
2017-11-06 22:04:26 +0100

    Add restart command for admin
    Only admins can use it /restart

--- animu_image_search_bot/bot.py
+++ animu_image_search_bot/bot.py
@@ -1,10 +1,13 @@
 import logging
+import os
+import sys
+from threading import Thread
 
 from telegram import Bot, TelegramError, Update
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
-from .commands import image_search_link, start, unknown, sticker_image_search
 from . import settings
+from .commands import image_search_link, start, sticker_image_search, unknown
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -25,8 +28,25 @@ def main():
     updater = Updater(settings.TELEGRAM_API_TOKEN)
     dispatcher = updater.dispatcher
 
+    def stop_and_restart():
+        """Gracefully stop the Updater and replace the current process with a new one."""
+        updater.stop()
+        os.execl(sys.executable, sys.executable, *sys.argv)
+
+    def restart(bot: Bot, update: Update):
+        """Start the restarting process
+
+        Args:
+            bot (:obj:`telegram.bot.Bot`): Telegram Api Bot Object.
+            update (:obj:`telegram.update.Update`): Telegram Api Update Object
+        """
+        update.message.reply_text('Bot is restarting...')
+        logger.info('Gracefully restarting...')
+        Thread(target=stop_and_restart).start()
+
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", start))
+    dispatcher.add_handler(CommandHandler('restart', restart, filters=Filters.user(username='@Nachtalb')))
 
     dispatcher.add_handler(MessageHandler(Filters.sticker, sticker_image_search))
     dispatcher.add_handler(MessageHandler(Filters.photo, image_search_link))
