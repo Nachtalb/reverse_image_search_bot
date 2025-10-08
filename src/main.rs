81af70c7b7d430e8736ca28831f5bd5a058f9028
Nachtalb <na@nachtalb.io>
2025-10-09 00:04:59 +0200

    fix: Graceful exit on missing TELOXIDE_TOKEN

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ async fn main() {
     pretty_env_logger::init();
     log::info!("Starting bot...");
 
+    if std::env::var("TELOXIDE_TOKEN").is_err() {
+        log::error!("TELOXIDE_TOKEN not set");
+        std::process::exit(1);
+    }
+
     bot::run().await;
 
     log::info!("Bot stopped");
